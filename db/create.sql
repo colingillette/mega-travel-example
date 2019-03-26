aab510b40dc2a5a3bee1fe0637ebfcbc2ef29bdf
Colin Gillette <cgillette@mfncuso.net>
2019-03-26 10:52:49 -0500

    table create script

--- db/create.sql
+++ db/create.sql
@@ -1,1 +1,20 @@
--- Columns: Name, phone, email, #adults, #children, destination, depart date, return date, activities
+-- Schema and table creation scripts pulled from MySQL Workbench
+-- These can be ran on any machine to replicate our simple database
+
+CREATE DATABASE `PortalDB` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+DROP TABLE `Reservations`;
+
+CREATE TABLE `Reservations` (
+    `reservation_id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(60) NOT NULL,
+    `phone` varchar(10) NOT NULL,
+    `email` varchar(60) NOT NULL,
+    `num_adults` int(2) NOT NULL,
+    `num_kids` int(2) NOT NULL,
+    `destination` varchar(45) NOT NULL,
+    `depart_date` datetime NOT NULL,
+    `return_date` datetime NOT NULL,
+    `activites` varchar(200),
+    PRIMARY KEY (`reservation_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
